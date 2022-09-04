d0366fcc3c838e626089e74ede7d155fcdce0344
Ashtyn Lane <anlane@student.fullsail.edu>
2022-09-04 11:13:49 -0500

    IDamageable and playerController

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] int HP;
+    [SerializeField] float playerSpeed;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float gravityValue;
+
+    [SerializeField] int jumpsMax;
+
+    [SerializeField] float shootRate;
+    [SerializeField] int shootDamage;
+    [SerializeField] int shootDistance;
+
+    int timesJumped;
+    private Vector3 playerVelocity;
+    Vector3 move;
+    bool isShooting;
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        movement();
+
+        StartCoroutine(shoot());
+    }
+
+    void movement()
+    {
+        if (controller.isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+            timesJumped = 0;
+        }
+
+        move = (transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (Input.GetButtonDown("Jump") && timesJumped < jumpsMax)
+        {
+            playerVelocity.y = jumpHeight;
+            timesJumped++;
+        }
+
+        playerVelocity.y -= gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    IEnumerator shoot()
+    {
+        if (!isShooting && Input.GetButton("Shoot"))
+        {
+            isShooting = true;
+
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(.5f, .5f)), out hit, shootDistance))
+            {
+                if (hit.collider.GetComponent<IDamageable>() != null)
+                {
+                    hit.collider.GetComponent<IDamageable>().takeDamage(shootDamage);
+                }
+            }
+
+            yield return new WaitForSeconds(shootRate);
+            isShooting = false;
+        }
+    }
+
+    public void takeDamage(int dmg)
+    {
+        HP -= dmg;
+        StartCoroutine(damageFlash());
+    }
+
+    IEnumerator damageFlash()
+    {
+      //  gameManager.instance.playerDamage.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        //gameManager.instance.playerDamage.SetActive(false);
+    }
+}
+
