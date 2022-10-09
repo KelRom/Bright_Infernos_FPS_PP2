3dddd9ae8f7f00d15ca27eff8fa9705030dd265c
Latrae <LMSwann@student.fullsail.edu>
2022-10-09 19:55:24 -0400

    Wizard boss, Ranged Sorcerer and Silas tweaks

--- Assets/Scripts/ai scripts/rangedEnemyAI.cs
+++ Assets/Scripts/ai scripts/rangedEnemyAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:182ef5a9f2e7710cba5581e0419c568f80a3fe96278ac4f4cef3d25c412ff75f
-size 4325
+oid sha256:8ba26209ee35993b5e1acb92bec102995733c81a86bda0201c4d876e37d95e72
+size 4323
