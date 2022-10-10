9bdaf44edd12dd9af385ed4c5afed5aba52a4645
Ryan Durham <rndurham@student.fullsail.edu>
2022-10-10 14:40:53 -0500

    Fixed UI Test Scene to be converted into Showchase Scene Fixed bug that restart would not work correctly if the continue button was selected (might be a feature and can be reverted)

--- Assets/Scripts/player Related/playerController.cs
+++ Assets/Scripts/player Related/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:146eb241a7908ceb041446c705b64b27dca5184f743058b0a5e4e5cc608beba3
-size 15653
+oid sha256:bbab35b86333e3c75aa0d06d20fc36d45a00a877fd5f9152e1290c690becba10
+size 15695
