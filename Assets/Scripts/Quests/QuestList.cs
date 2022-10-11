b57382dbca9d05414d503097c2060737a6e54598
Ryan Durham <rndurham@student.fullsail.edu>
2022-10-10 19:12:47 -0500

    Fixed win condition to work correctly

--- Assets/Scripts/Quests/QuestList.cs
+++ Assets/Scripts/Quests/QuestList.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de7265df790522ee2575b5cbbf272e5c67912b747eae1bf48421825f03a02f13
-size 2878
+oid sha256:db0f578e5820998bd412ed3761876636ab6646f8566b2f3cc340aae73f8d239d
+size 3042
