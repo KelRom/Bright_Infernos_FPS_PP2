676947d4903dee29adce57da60106123709c6f25
Ashtyn Lane <anlane@student.fullsail.edu>
2022-10-13 16:22:17 -0500

    added cave bosses, spawners to forest, started checkpoints

---
+++ Assets/Scripts/Saving/Checkpoint.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0e3631894834e69075b0880c132521a720a1a0c2977c08d99e5df6426cd1fad8
+size 671
--- Assets/Scripts/ai scripts/meleeEnemyAI.cs
+++ Assets/Scripts/ai scripts/meleeEnemyAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d3a1392eb88009ab6d2a443b07cd5f30d6a090bab4f9a37ad103dedfe63deb4
-size 9936
+oid sha256:0cfa6f4eb30b545ba00b2c4822dbdf049d83682799bcffba46d7e1cbc92671bc
+size 9942
