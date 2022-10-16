c2d552e158252790db85ae3d7679ae209063cc25
kel <kromandomench@student.fullsail.edu>
2022-10-15 19:54:46 -0500

    Fixed freeze issue when player is knocked back

--- Assets/Scripts/player Related/playerController.cs
+++ Assets/Scripts/player Related/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1eca38ab818fb5c79656b77ee14870be95dceebb93d3e0bd264d92a8d41e0ec
-size 17201
+oid sha256:2b161e8fa2d26382fd155a473564e43a2a04bf4f2c1452c5683ce58a31f18a33
+size 17193
