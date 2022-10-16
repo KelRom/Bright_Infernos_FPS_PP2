e23459234fa037ce386c76679bf7ce3c5d2a7796
KCi Herzer <neonhippie.kci@gmail.com>
2022-10-16 15:39:45 -0700

    enemyMovement updates

--- Assets/Scripts/ai scripts/enemyMovement.cs
+++ Assets/Scripts/ai scripts/enemyMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4808ee6864b60257519e405fc609cff3595f74b0336fc10b2b3ce47477a19c7
-size 4027
+oid sha256:1f541c1a2804464b00ffc5e037c6b79b0ffb427c65b0ddd698170ef08eeae88c
+size 4998
