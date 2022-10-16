9198555e1bdb09a6ba81bb52d3f1019577583225
kel <kromandomench@student.fullsail.edu>
2022-10-15 20:22:16 -0500

    Knockback maybe?

--- Assets/Scripts/player Related/playerController.cs
+++ Assets/Scripts/player Related/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b161e8fa2d26382fd155a473564e43a2a04bf4f2c1452c5683ce58a31f18a33
-size 17193
+oid sha256:9c6736ce423f586c7c83bab5fa7e2b7f894b089a45d0e01a68d4c2bbf05305e2
+size 17249
