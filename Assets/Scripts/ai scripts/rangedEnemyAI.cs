0afc7dfb206e2822e56f16a0db7440d3838d9e4b
Ashtyn Lane <anlane@student.fullsail.edu>
2022-10-16 21:34:17 -0500

    ranged soldier and bandit

--- Assets/Scripts/ai scripts/rangedEnemyAI.cs
+++ Assets/Scripts/ai scripts/rangedEnemyAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76d7de3482bb1485c22bba3419b3322fd9c9af95ef6b173fbd040b7c29288bf8
-size 5215
+oid sha256:41523bc92be7a2492f20457365533affd98c1e60f70174477ee1ac4a9b3e6843
+size 5279
