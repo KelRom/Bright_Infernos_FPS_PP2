1425dbfc5186533c9232983e33a8322c2b9ce9d2
Ashtyn Lane <anlane@student.fullsail.edu>
2022-10-17 08:07:37 -0500

    modified knockback

--- Assets/Scripts/player Related/playerController.cs
+++ Assets/Scripts/player Related/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e82bd89266e925120e0b1fc6ea0701970b97a9e616d36b0e08e22b61d7a4bc21
-size 17760
+oid sha256:89c30f324735cdcf8d36e59e6dd88eeeaa2b0b3aca028074648bac1143510980
+size 17752
