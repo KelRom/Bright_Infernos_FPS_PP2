aaffd0d6460fc1ac5ce8058d0f5ae14bce79efc1
Latrae <LMSwann@student.fullsail.edu>
2022-10-16 20:58:54 -0400

    Restoration Spell in the game and is supposed to be fully operational. Particle effect is ugly though.

--- Assets/Scripts/player Related/playerController.cs
+++ Assets/Scripts/player Related/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b129a2ad5c6ae2d7a02c2b60349cfa1ee4de8f81ef3485b0f05f2787861068a4
-size 17016
+oid sha256:070986681fdc02bfbd3b05af7282547d0aeea7532c0680d365c79c74d7ec7d9e
+size 17526
