c7189cc02cc4287eab176fdfc27ab1ac163e2b21
KCi Herzer <neonhippie.kci@gmail.com>
2022-10-17 09:52:08 -0700

    minor changes

--- Assets/Scripts/ai scripts/TriggerSphere.cs
+++ Assets/Scripts/ai scripts/TriggerSphere.cs
@@ -1,15 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TriggerSphere : MonoBehaviour
-{
-    private void OnTriggerEnter(Collider other)
-    {
-        GetComponentInParent<meleeEnemyAI>().SphereTriggerEnter(other);
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        GetComponentInParent<meleeEnemyAI>().SphereTriggerExit(other);
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:5e6a43fe65ca3e7778da24bf256f4c6c3cde52f853dca2198b8ad54cd8f02923
+size 910
--- Assets/Scripts/ai scripts/rangedEnemyAI.cs
+++ Assets/Scripts/ai scripts/rangedEnemyAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54e046db904c137a667cf1e054e966f85354d9c93e8d7462bf8d8b8e0372a7b0
-size 5276
+oid sha256:b4d291ca742393caaba9dc72f75ec6d9d5a9cd36220f664cd98121268374eb95
+size 5280
