3b6761e504353bedb086229353e66b9fd688d830
kel <kromandomench@student.fullsail.edu>
2022-10-16 20:25:26 -0500

    Removed showcase level, changed image for back button.

--- Assets/Scripts/UI scripts/buttonFunctions.cs
+++ Assets/Scripts/UI scripts/buttonFunctions.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac9ec480ce0e736e8d66923a485f4346e368ec25740697f06c18a10ce89ffe3a
-size 4362
+oid sha256:9cf88b04166d8d2dba7ea0f57755f681b71078c229794df4a5dc208f28fe73f2
+size 4282
