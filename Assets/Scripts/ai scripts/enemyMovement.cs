df4f9737880d76081f00c8d13a3b022b5d1d3b17
KCi Herzer <neonhippie.kci@gmail.com>
2022-10-17 09:41:08 -0700

    Enemy prefab changes

--- Assets/Scripts/ai scripts/enemyMovement.cs
+++ Assets/Scripts/ai scripts/enemyMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f541c1a2804464b00ffc5e037c6b79b0ffb427c65b0ddd698170ef08eeae88c
-size 4998
+oid sha256:2f8ef991057dc2c56a60494c910f83374b05e9dac1c121e35d56b1e588b4e68a
+size 5034
