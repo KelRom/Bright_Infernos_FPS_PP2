be9f3ec88fd80ed1244c4222fee6ba6bd35bebc9
kel <kromandomench@student.fullsail.edu>
2022-10-17 21:26:20 -0500

    removed player warnings

--- Assets/Scripts/player Related/playerController.cs
+++ Assets/Scripts/player Related/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e539fca6378bf6b98b70bb2e18de063a2c08d7d48c245cc8a077ea2b4d54cf90
-size 17959
+oid sha256:7809bef50ff55c1cb1619fb2ab1a90227c99f5e2c129e34722d6def2e1e963b1
+size 17570
