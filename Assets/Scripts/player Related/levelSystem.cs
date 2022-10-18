73ebdb1d2fbbb10121eebd91275b52961307b4dc
Ryan Durham <rndurham@student.fullsail.edu>
2022-10-17 19:24:48 -0500

    SceneTransition prefab update and the start of the saving the levelSystem.

--- Assets/Scripts/player Related/levelSystem.cs
+++ Assets/Scripts/player Related/levelSystem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13e516a960878cb2d2b70a706e4c9fbd67b6256985780b037134512f501f4cad
-size 1493
+oid sha256:f615744f58c8555a1b9ffcdde6687b6bf190d0d97b5914ab217bd01b30afc9e5
+size 2098
