0fa0c84488437191bb095d68cef8b84ae286c5ab
Ashtyn Lane <anlane@student.fullsail.edu>
2022-10-17 19:16:17 -0500

    added spells to ranged enemies, made sure movement was set up for all enemies

--- Assets/Scripts/ai scripts/rangedEnemyAI.cs
+++ Assets/Scripts/ai scripts/rangedEnemyAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4d291ca742393caaba9dc72f75ec6d9d5a9cd36220f664cd98121268374eb95
-size 5280
+oid sha256:6b8fb04f8ebd5d2b426f0f3c26c67b31db57533f12ee3f4cc86cd8ae9a0a6367
+size 5352
