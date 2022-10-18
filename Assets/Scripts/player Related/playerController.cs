d57551fe2d4717f8b8afb3a413d467eeeb5aa2e6
Ashtyn Lane <anlane@student.fullsail.edu>
2022-10-17 20:47:56 -0500

    changed silas to start with sword

--- Assets/Scripts/player Related/playerController.cs
+++ Assets/Scripts/player Related/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:584b751b29e4b1347e9835f42e5916a391031eec326248eeae85854e51d58082
-size 17968
+oid sha256:e539fca6378bf6b98b70bb2e18de063a2c08d7d48c245cc8a077ea2b4d54cf90
+size 17959
