794db8ad9348c8baf35456ff3aae94a6dfb65214
kel <kromandomench@student.fullsail.edu>
2022-10-19 19:25:03 -0500

    Fixed door animation randomly opening in village

--- Assets/Scripts/Magic System/MagicGameObject.cs
+++ Assets/Scripts/Magic System/MagicGameObject.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3d75d2e05be9321f7f48517d445ebed57edcb5bf2c152e884fa331a14295514
-size 1836
+oid sha256:c9304be663b048818f4a0a1ef1041d293f3b30e34758e7c2f5c4a33c7d65eefe
+size 1843
--- Assets/Scripts/player Related/playerController.cs
+++ Assets/Scripts/player Related/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7809bef50ff55c1cb1619fb2ab1a90227c99f5e2c129e34722d6def2e1e963b1
-size 17570
+oid sha256:ed47d8eda8961df19ad8f328c0c7311e8a743d84d76ba9726e2fe5957d92c6cb
+size 17668
