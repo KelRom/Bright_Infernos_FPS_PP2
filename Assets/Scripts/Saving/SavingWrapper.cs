de169ac6f14297f66bb0da9dff5982fa13f87dba
kel <kromandomench@student.fullsail.edu>
2022-10-20 21:13:58 -0500

    Fixed issue where you can press b to load back to previous spot

--- Assets/Scripts/Saving/SavingWrapper.cs
+++ Assets/Scripts/Saving/SavingWrapper.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79d2f0505618a636870b67deb0aa1f665e83a91a47b2f21211d73aa3722ec19b
-size 1059
+oid sha256:63f4a2a9eae4118ffb4fb7a9eaba8fee2d76f2051ccb3cf9fd338cae39d04243
+size 1075
