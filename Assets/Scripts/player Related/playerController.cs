8deb5737ee7234a4e098225809be57ba6c1466fb
kel <kromandomench@student.fullsail.edu>
2022-10-21 17:53:15 -0500

    Fixed mana regen

--- Assets/Scripts/player Related/playerController.cs
+++ Assets/Scripts/player Related/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed47d8eda8961df19ad8f328c0c7311e8a743d84d76ba9726e2fe5957d92c6cb
-size 17668
+oid sha256:52b991f0658a38c8b5fade4df54354d07a74db515c70443b19ebc87c9d68dabd
+size 17751
