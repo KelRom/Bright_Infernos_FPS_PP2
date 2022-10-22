cee4bba97a082c8defdf4a06d308c83a82f262fd
Ashtyn Lane <anlane@student.fullsail.edu>
2022-10-21 19:50:22 -0500

    terrain adjustment rebake

--- Assets/Scripts/ai scripts/npcAI.cs
+++ Assets/Scripts/ai scripts/npcAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:532c70587646d12f300207938c169a3aef9f56f4aba93fee419eb3f98469cb3c
-size 2167
+oid sha256:643730cec3e6bc7afb4ad87e346be43efadebe402f9cad794d78a94f6dc3bb43
+size 2321
