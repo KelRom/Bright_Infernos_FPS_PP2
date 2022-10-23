e3a59b452abe4ff05c92d25616c5f9b12d52463a
Latrae <LMSwann@student.fullsail.edu>
2022-10-23 12:50:20 -0400

    Spell cooldown UI display completed.

--- Assets/Scripts/UI scripts/gameManager.cs
+++ Assets/Scripts/UI scripts/gameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07d88af4c069adeca05fe36336aff829a439f479e137bae24f7f51b47e33edb4
-size 5061
+oid sha256:68b8ecb219003a3a33236dde1835a1b826f5831cc9fa7bc18090cfbfa254a546
+size 5096
--- Assets/Scripts/player Related/playerController.cs
+++ Assets/Scripts/player Related/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8053b63dbe2e07a13e985a2fce162059362e00df3527129efc9be8341a28e637
-size 18306
+oid sha256:2dd9552a7538c3ab7c2145b6c165cfcef9b6618df26b5d5dd361ac80e69b2aa0
+size 19063
