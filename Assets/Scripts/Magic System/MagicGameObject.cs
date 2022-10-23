cfdcbab48f15eed305ffdcfd8f2915ea86466592
Ryan Durham <rndurham@student.fullsail.edu>
2022-10-22 23:52:30 -0500

    Spells save between scenes

--- Assets/Scripts/Magic System/MagicGameObject.cs
+++ Assets/Scripts/Magic System/MagicGameObject.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58435be7e64dccefe275e472ac10e4d9644c8d8e9289ff66b17b8dc0a48f7667
-size 1781
+oid sha256:e7b4d8559f5b576d88eb751fa7bd8f5707ceb3e3e3f5e64a19ec64bf3dbddd99
+size 1783
--- Assets/Scripts/player Related/playerController.cs
+++ Assets/Scripts/player Related/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54577ef8657c28de72025eed410c3898b33b9d8e83da38cf7a95dc85013fe550
-size 17690
+oid sha256:3ea56101ad7423dc00284ebf7ca65beb03dddfb0dd82004bc8db073399ee808f
+size 18223
