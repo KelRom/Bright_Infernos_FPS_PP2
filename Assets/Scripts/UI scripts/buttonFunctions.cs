fadd0b40f26af943fd7d29fb009d8b315178fbeb
Ryan Durham <rndurham@student.fullsail.edu>
2022-10-23 00:08:57 -0500

    Fixed issue when continuing in the cave the player would not load in the correct location,

--- Assets/Scripts/UI scripts/buttonFunctions.cs
+++ Assets/Scripts/UI scripts/buttonFunctions.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2147e3c16e33c6163fb601fbba6f85af6935a6350dea59b9e496453abef48a1
-size 4571
+oid sha256:fecd64bb44eb35491ee672f47a501d2d74148128a82483cad4e0665238834b42
+size 4577
--- Assets/Scripts/player Related/playerController.cs
+++ Assets/Scripts/player Related/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ea56101ad7423dc00284ebf7ca65beb03dddfb0dd82004bc8db073399ee808f
-size 18223
+oid sha256:8053b63dbe2e07a13e985a2fce162059362e00df3527129efc9be8341a28e637
+size 18306
